3ce75d8ae18202c9b0b157b6ed450b9a5aef3592
luisbrunod <bruno.david@thelabtech.com.ar>
2024-06-27 22:10:17 -0300

    add asientos and comment

--- dml_inserts.sql
+++ dml_inserts.sql
@@ -50,39 +50,6 @@ VALUES (0, 1, 1,60000),
        (1, 3, 5,65000),
        (0, 3, 1,70000),
 
--- Asientos hasta A10, B10 y C10 en Platea baja 1
-INSERT INTO Asiento (fila, columna, esta_vendido, seccionID)
-VALUES ('A', '1', 0, 3),
-	   ('A', '2', 0, 3),
-	   ('A', '3', 0, 3),
-	   ('A', '4', 0, 3),
-	   ('A', '5', 0, 3),
-	   ('A', '6', 0, 3),
-	   ('A', '7', 0, 3),
-	   ('A', '8', 0, 3),
-	   ('A', '9', 0, 3),
-	   ('B', '10', 0, 3),
-	   ('B', '1', 0, 3),
-	   ('B', '2', 0, 3),
-	   ('B', '3', 0, 3),
-	   ('B', '4', 0, 3),
-	   ('B', '5', 0, 3),
-	   ('B', '6', 0, 3),
-	   ('B', '7', 0, 3),
-	   ('B', '8', 0, 3),
-	   ('B', '9', 0, 3),
-	   ('B', '10', 0, 3),
-	   ('C', '1', 0, 3),
-	   ('C', '2', 0, 3),
-	   ('C', '3', 0, 3),
-	   ('C', '4', 0, 3),
-	   ('C', '5', 0, 3),
-	   ('C', '6', 0, 3),
-	   ('C', '7', 0, 3),
-	   ('C', '8', 0, 3),
-	   ('C', '9', 0, 3),
-	   ('C', '10', 0, 3);
-
 INSERT INTO Ubicacion_asiento (fila, columna)
 VALUES
     ('A', 1),
@@ -147,146 +114,56 @@ VALUES
     ('F', 10);
 
 -- Asientos hasta A10, B10 y C10 en Platea baja 2
-INSERT INTO Asiento (fila, columna, esta_vendido, seccionID)
-VALUES ('A', '1', 0, 4),
-	   ('A', '2', 0, 4),
-	   ('A', '3', 0, 4),
-	   ('A', '4', 0, 4),
-	   ('A', '5', 0, 4),
-	   ('A', '6', 0, 4),
-	   ('A', '7', 0, 4),
-	   ('A', '8', 0, 4),
-	   ('A', '9', 0, 4),
-	   ('B', '10', 0, 4),
-	   ('B', '1', 0, 4),
-	   ('B', '2', 0, 4),
-	   ('B', '3', 0, 4),
-	   ('B', '4', 0, 4),
-	   ('B', '5', 0, 4),
-	   ('B', '6', 0, 4),
-	   ('B', '7', 0, 4),
-	   ('B', '8', 0, 4),
-	   ('B', '9', 0, 4),
-	   ('B', '10', 0, 4),
-	   ('C', '1', 0, 4),
-	   ('C', '2', 0, 4),
-	   ('C', '3', 0, 4),
-	   ('C', '4', 0, 4),
-	   ('C', '5', 0, 4),
-	   ('C', '6', 0, 4),
-	   ('C', '7', 0, 4),
-	   ('C', '8', 0, 4),
-	   ('C', '9', 0, 4),
-	   ('C', '10', 0, 4);
+INSERT INTO Asiento (esta_vendido, seccionID, ubicacionID)
+SELECT
+    0, -- 0 para indicar que no está vendido
+    ABS(CHECKSUM(NEWID())) % 6 + 1, -- Valor aleatorio entre 1 y 6
+    ubicacionID
+FROM Ubicacion_asiento;
 
--- Asientos hasta A10, B10 y C10 en Platea alta 1
-INSERT INTO Asiento (fila, columna, esta_vendido, seccionID)
-VALUES ('A', '1', 0, 5),
-	   ('A', '2', 0, 5),
-	   ('A', '3', 0, 5),
-	   ('A', '4', 0, 5),
-	   ('A', '5', 0, 5),
-	   ('A', '6', 0, 5),
-	   ('A', '7', 0, 5),
-	   ('A', '8', 0, 5),
-	   ('A', '9', 0, 5),
-	   ('B', '10', 0, 5),
-	   ('B', '1', 0, 5),
-	   ('B', '2', 0, 5),
-	   ('B', '3', 0, 5),
-	   ('B', '4', 0, 5),
-	   ('B', '5', 0, 5),
-	   ('B', '6', 0, 5),
-	   ('B', '7', 0, 5),
-	   ('B', '8', 0, 5),
-	   ('B', '9', 0, 5),
-	   ('B', '10', 0, 5),
-	   ('C', '1', 0, 5),
-	   ('C', '2', 0, 5),
-	   ('C', '3', 0, 5),
-	   ('C', '4', 0, 5),
-	   ('C', '5', 0, 5),
-	   ('C', '6', 0, 5),
-	   ('C', '7', 0, 5),
-	   ('C', '8', 0, 5),
-	   ('C', '9', 0, 5),
-	   ('C', '10', 0, 5);
 
--- Asientos hasta A10, B10 y C10 en Platea alta 2
-INSERT INTO Asiento (fila, columna, esta_vendido, seccionID)
-VALUES ('A', '1', 0, 6),
-	   ('A', '2', 0, 6),
-	   ('A', '3', 0, 6),
-	   ('A', '4', 0, 6),
-	   ('A', '5', 0, 6),
-	   ('A', '6', 0, 6),
-	   ('A', '7', 0, 6),
-	   ('A', '8', 0, 6),
-	   ('A', '9', 0, 6),
-	   ('B', '10', 0, 6),
-	   ('B', '1', 0, 6),
-	   ('B', '2', 0, 6),
-	   ('B', '3', 0, 6),
-	   ('B', '4', 0, 6),
-	   ('B', '5', 0, 6),
-	   ('B', '6', 0, 6),
-	   ('B', '7', 0, 6),
-	   ('B', '8', 0, 6),
-	   ('B', '9', 0, 6),
-	   ('B', '10', 0, 6),
-	   ('C', '1', 0, 6),
-	   ('C', '2', 0, 6),
-	   ('C', '3', 0, 6),
-	   ('C', '4', 0, 6),
-	   ('C', '5', 0, 6),
-	   ('C', '6', 0, 6),
-	   ('C', '7', 0, 6),
-	   ('C', '8', 0, 6),
-	   ('C', '9', 0, 6),
-	   ('C', '10', 0, 6);
-
-INSERT INTO Compra (fechaCompra, DNI)
-VALUES ('2024-05-24', '12345678'),
-		('2024-05-12', '34567890'),
-		('2024-03-01', '45678901'), -- mismo cliente hace 2 compras en distintos dias
-		('2024-02-05', '45678901'),
-		('2024-02-28', '89012345'),
-		('2024-03-23', '67890123'),
-		('2024-04-27', '90123456');
+--INSERT INTO Compra (fechaCompra, DNI)
+--VALUES ('2024-05-24', '12345678'),
+--		('2024-05-12', '34567890'),
+--		('2024-03-01', '45678901'), -- mismo cliente hace 2 compras en distintos dias
+--		('2024-02-05', '45678901'),
+--		('2024-02-28', '89012345'),
+--		('2024-03-23', '67890123'),
+--		('2024-04-27', '90123456');
 
 -- Entradas de Campo Delantero
-INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
-VALUES  (1, 1, null, 1, 1, '12345678', 150.00), -- misma entrada para misma banda, seccion y dueño
-		(1, 1, null, 1, 1, '34567890', 250.00),
-		(2, 1, null, 1, 1, '32067890', 250.00),
-		(3, 1, null, 1, 1, '39867890', 250.00);
+--INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
+--VALUES  (1, 1, null, 1, 1, '12345678', 150.00), -- misma entrada para misma banda, seccion y dueño
+--		(1, 1, null, 1, 1, '34567890', 250.00),
+--		(2, 1, null, 1, 1, '32067890', 250.00),
+--		(3, 1, null, 1, 1, '39867890', 250.00);
 
 -- Entradas de  Campo Trasero
-INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
-VALUES  (1, 1, null, 2, 1, '23456789', 200.00),
-		(3, 1, null, 2, 1, '43456789', 200.00),
-		(4, 1, null, 2, 1, '14345678', 150.00);
+--INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
+--VALUES  (1, 1, null, 2, 1, '23456789', 200.00),
+--		(3, 1, null, 2, 1, '43456789', 200.00),
+--		(4, 1, null, 2, 1, '14345678', 150.00);
 
 -- Entradas de  Platea baja 1
-INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
-VALUES  (3, 1, 1, 3, 1, '23454789', 300.00),
-		(4, 1, 2, 3, 1, '12345478', 450.00);
+--INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
+--VALUES  (3, 1, 1, 3, 1, '23454789', 300.00),
+--		(4, 1, 2, 3, 1, '12345478', 450.00);
 
 -- Entradas de  Platea baja 2
-INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
-VALUES  (3, 1, 2, 4, 1, '43454749', 400.00),
-		(4, 1, 3, 4, 1, '12345466', 450.00);
+--INSERT INTO Entrada (compraID, bandaID, asientoID, seccionID, estadioID, DNI_propietario, precio)
+--VALUES  (3, 1, 2, 4, 1, '43454749', 400.00),
+--		(4, 1, 3, 4, 1, '12345466', 450.00);
 
 --------------------------------
 
 -- Verificaciones
-select * from Cliente
-select * from Estadio
-select * from Banda
-select * from Seccion
-select * from Asiento
-select * from Compra
-select * from Entrada
+--select * from Cliente
+--select * from Estadio
+--select * from Banda
+--select * from Seccion
+--select * from Asiento
+--select * from Compra
+--select * from Entrada
 
 
 
