304c6df0393000aba1492e25f34edf4d940227ad
sofiapel <93402677+DavidLBruno@users.noreply.github.com>
2024-06-29 13:58:57 -0300

    function exercise number 6

---
+++ function6.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION TotalRecaudado (@bandaID INT, @fecha_1 DATE, @fecha_2 DATE)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @totalRecaudado INT;
+    
+    SELECT @totalRecaudado = ISNULL(SUM(precio),0)
+    FROM Entrada e
+    WHERE recitalID IN (
+        SELECT recitalID
+        FROM Recital
+        WHERE fecha BETWEEN @fecha_1 AND @fecha_2 AND bandaID = @bandaID
+    );
+
+    RETURN @totalRecaudado;
+END;
+GO;
+
+
+SELECT dbo.TotalRecaudado(1,'2022-06-01','2024-08-08');
